2fb5010895144362d66d5fb42197a0ac7e8deac5
AmineAboutaieb <amine.aboutaieb@gmail.com>
2022-07-24 04:17:46 +0100

    added try catch in categories scraping script

--- src/site_category_scraper.py
+++ src/site_category_scraper.py
@@ -1,61 +1,62 @@
+from importlib.resources import path
+from unittest.mock import patch
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 import time
 import json
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
+options = webdriver.ChromeOptions()
+options.add_argument("--ignore-certificate-error")
+options.add_argument("--ignore-ssl-errors")
 
-driver = webdriver.Chrome(PATH)
+PATH = "./chromedriver.exe"
+try:
+    driver = webdriver.Chrome(PATH,chrome_options=options)
 
-driver.get("https://www.jumia.ma")
+    time.sleep(4)
 
-time.sleep(2)
+    driver.get("https://www.jumia.ma/")
 
-driver.refresh()
-
-
-container = driver.find_element(by=By.CLASS_NAME, value="flyout")
-
-hover = ActionChains(driver).move_to_element(container)
-hover.perform()
-
-time.sleep(2)
-
-s_item_elements = driver.find_elements(by=By.CLASS_NAME, value="s-itm")
-tit_elements = driver.find_elements(by=By.CLASS_NAME, value="tit")
-
-elements = s_item_elements + tit_elements
-
-categories = []
-
-print("Scraping categories...")
-
-for element in elements:
-    href = element.get_attribute("href")
-    if(href and ("https://www.jumia.ma/" in href) and href.find("tvs/?display_size") == -1 ):
-        name = href.split("//")[1].split("/")[1].replace("-", "_")
-        f_href = href.split("/")[3]
-        categories.append({"name" : name, "link" : f_href})
+    driver.refresh()
 
 
+    container = driver.find_element(by=By.CLASS_NAME, value="flyout")
 
-f = open("categories.json", "w")
-json.dump(categories, f)
-f.close()
+    hover = ActionChains(driver).move_to_element(container)
+    hover.perform()
 
-print("Created categories.json file...")
+    time.sleep(2)
 
-print("All done")
+    s_item_elements = driver.find_elements(by=By.CLASS_NAME, value="s-itm")
+    tit_elements = driver.find_elements(by=By.CLASS_NAME, value="tit")
 
-print("Scraped {} categories".format(len(categories)))
+    elements = s_item_elements + tit_elements
 
-driver.quit()
+    categories = []
+
+    print("Scraping categories...")
+
+    for element in elements:
+        href = element.get_attribute("href")
+        if(href and ("https://www.jumia.ma/" in href) and href.find("tvs/?display_size") == -1 ):
+            name = href.split("//")[1].split("/")[1].replace("-", "_")
+            f_href = href.split("/")[3]
+            categories.append({"name" : name, "link" : f_href})
 
 
 
+    f = open("categories.json", "w")
+    json.dump(categories, f)
+    f.close()
 
+    print("Created categories.json file...")
 
+    print("All done")
 
+    print("Scraped {} categories".format(len(categories)))
 
+    driver.quit()
 
+except Exception as exc:
+    print(exc)
